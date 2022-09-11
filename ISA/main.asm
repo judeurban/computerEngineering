e78418c26ff3cd157e0c9f73ebecc4a200553aee
ceyda <64362304+ceydaekaya@users.noreply.github.com>
2022-09-11 11:39:38 -0500

    Update
    Tiny correction on equation.

--- ISA/main.asm
+++ ISA/main.asm
@@ -3,15 +3,16 @@
 # pseudocode to calculate the ratio of pi:
 # diameter = 10.0
 # circumference = 3.18309886184
-# pi = diameter / circumference
+# pi = circumference / diameter c=2pir > pi=c/R
 
-# r1 is the diameter
-# r2 is the circumference
+# r1 is the circumference
+# r2 is the diameter
 # r3 is the result of pi
 
 # load the status values into the registers
-loadf(r1, 10.0)                  # diameter
-loadf(r2, 3.18309886184)         # circumference
+loadf(r1, 3.18309886184)                  # circumference
+loadf(r2, 10.0)                           # diameter
 
 div(r3, r1, r2)                 # calculate pi
 console(r3)                     # print the value in r3 to the console
+
